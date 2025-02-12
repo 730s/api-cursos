0d5c11db1ef74aaead4867109b1ff8967e9d8ffb
730s <leonardosade@gmail.com>
2025-02-12 19:28:20 -0300

    Criação dos repositórios

---
+++ src/main/java/com/leo/apicursos/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,13 @@
+package com.leo.apicursos.modules.company.repositories;
+
+import com.leo.apicursos.modules.company.entities.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+
+
+}
---
+++ src/main/java/com/leo/apicursos/modules/company/repositories/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.leo.apicursos.modules.company.repositories;
+
+import com.leo.apicursos.modules.company.entities.CourseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CourseRepository extends JpaRepository <CourseEntity, UUID> {
+}
